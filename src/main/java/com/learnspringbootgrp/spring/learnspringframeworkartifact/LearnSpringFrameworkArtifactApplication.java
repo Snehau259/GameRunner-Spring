eb087a07dd7ffe3df8e35f4ee97e9cf35eec01d0
snehau259 <snehau259@gmail.com>
2023-01-31 13:21:57 +0530

    initial commit

---
+++ src/main/java/com/learnspringbootgrp/spring/learnspringframeworkartifact/LearnSpringFrameworkArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringbootgrp.spring.learnspringframeworkartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringbootgrp/spring/learnspringframeworkartifact/LearnSpringFrameworkArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringbootgrp.spring.learnspringframeworkartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
