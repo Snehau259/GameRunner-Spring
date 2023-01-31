2dee61aed7e2017c77ae6e08b729ec9d81420d40
snehau259 <snehau259@gmail.com>
2023-01-31 14:01:51 +0530

    [Sneha] add. superContraGame tightcoupling

---
+++ src/main/java/com/learnspringbootgrp/spring/learnspringframeworkartifact/game/SuperContraGame.java
@@ -0,0 +1,24 @@
+package com.learnspringbootgrp.spring.learnspringframeworkartifact.game;
+
+public class Mariogame {
+
+    public void up()
+    {
+        System.out.println("up");
+    }
+
+    public void down()
+    {
+        System.out.println("down");
+    }
+
+    public void right()
+    {
+        System.out.println("right");
+    }
+
+    public void left()
+    {
+        System.out.println("left");
+    }
+}
